00be64b76d87cee61db4040779fc520a055cf0c3
Rafael Henrique da Silva <rafaelsilva192@gmail.com>
2016-09-27 17:19:43 -0300

    feat(Elfo, Item) : Adicionar Lógica inicial Elfos e Itens

---
+++ Bluej projeto Modulo 1/Elfo.java
@@ -0,0 +1,31 @@
+public class Elfo
+{
+    private String nome;
+    private Item arco;
+    private Item flechas;
+    private int experiencia;
+
+    public Elfo(String n){
+        nome = n;
+        arco = new Item("Arco", 1);
+        flechas = new Item("Flechas", 42);
+    }
+
+    public void setNome(String n){
+        nome = n;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void atirarFlecha(){
+        flechas.setQuantidade(flechas.getQuantidade()-1);
+        experiencia++;
+    }
+
+    /*public void atirarFlechaRefactory(){
+        experiencia++;
+        flechas.setQuantidade(flechas.getQuantidade()-1);
+    }*/
+}
