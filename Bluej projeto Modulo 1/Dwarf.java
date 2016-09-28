67ddf49f9efbeae11a21c92ffb698b62d333acfa
Unknown <rafaelsilva192@gmail.com>
2016-09-28 10:10:40 -0300

    feat(Dwarf, Elfo): adicionado a possibilidade de caçar dwarfs

---
+++ Bluej projeto Modulo 1/Dwarf.java
@@ -0,0 +1,15 @@
+public class Dwarf
+{
+    // instance variables - replace the example below with your own
+    private int vida;
+    public Dwarf()
+    {
+        vida = 110;
+    }
+    public void setVida(int v){
+        vida = v;
+    }
+    public int getVida(){
+        return vida;
+    }
+}
--- Bluej projeto Modulo 1/Elfo.java
+++ Bluej projeto Modulo 1/Elfo.java
@@ -4,11 +4,12 @@ public class Elfo
     private Item arco;
     private Item flechas;
     private int experiencia;
-
+    private Dwarf vida;
     public Elfo(String n){
         nome = n;
         arco = new Item("Arco", 1);
         flechas = new Item("Flechas", 42);
+        vida = new Dwarf();
     }
 
     public void setNome(String n){
@@ -25,12 +26,26 @@ public class Elfo
     public Item getFlechas(){
         return flechas;
     }
+    public Dwarf getVida(){
+        return vida;
+    }
     public void atirarFlecha(){
         if(flechas.getQuantidade() == 0){     
         }
         else{
         flechas.setQuantidade(flechas.getQuantidade()-1);
         experiencia++;
+    }
+    }
+     public void atirarFlechaNoDwarf(){
+        if(flechas.getQuantidade() == 0){}
+        else{
+        flechas.setQuantidade(flechas.getQuantidade()-1);
+        if(vida.getVida() == 0){}
+        else{
+        vida.setVida(vida.getVida()-10);
+        experiencia++;
+    }
     }
     }
 
