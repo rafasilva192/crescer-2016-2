4ef08337be1867b23e8dee673bd4919da9711aa6
Unknown <rafaelsilva192@gmail.com>
2016-09-29 18:26:38 -0300

    Feat(Inventario): Adicionar inventario e a possibilidade de colocar e tirar itens dele

--- Bluej projeto Modulo 1/Inventario.java
+++ Bluej projeto Modulo 1/Inventario.java
@@ -4,7 +4,16 @@ public class Inventario
 {
     ArrayList<Item> itens = new ArrayList<>();
 
-    public void adicionarItem(Item descricao, Item quantidade){
+    public void adicionarItem(String descricao, int quantidade){        
        itens.add(new Item(descricao, quantidade));
+    }
+    
+    public void removerItem(String descricao){
+        for(int i = 0; i < itens.size(); i++){
+            Item itemAtual = itens.get(i);
+            if(descricao.equals(itemAtual.getDescricao())){
+                itens.remove(descricao);
+            }
+        }
     }
 }
---
+++ Bluej projeto Modulo 1/InventarioTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class InventarioTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class InventarioTest
+{
+    /**
+     * Default constructor for test class InventarioTest
+     */
+    public InventarioTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
