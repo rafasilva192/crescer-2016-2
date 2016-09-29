718a3d01125699ca5934d696e6287c3ba2dd9209
Unknown <rafaelsilva192@gmail.com>
2016-09-29 08:06:30 -0300

    Feat(DataTerceiraEra): Adicionar uma nova classe que armazena uma data

---
+++ Bluej projeto Modulo 1/DataTerceiraEra.java
@@ -0,0 +1,17 @@
+
+public class DataTerceiraEra
+{
+    private int dia;
+    private int mes;
+    private int ano;
+
+
+    public DataTerceiraEra(int dia, int mes, int ano)
+    {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+
+}
