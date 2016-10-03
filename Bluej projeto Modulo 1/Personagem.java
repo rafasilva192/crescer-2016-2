0e93b4095b3d24bb80f2f74b3704d289f984195a
Rafael Henrique da Silva <rafaelsilva192@gmail.com>
2016-10-03 17:15:02 -0300

    Feat(Personagem): Adicionar classe personagem daonde as classes elfos, dwarf e irishdwarf herdam informações.

---
+++ Bluej projeto Modulo 1/Personagem.java
@@ -0,0 +1,35 @@
+
+public class Personagem
+{
+    protected String nome;
+    protected Inventario inventario;
+    protected int experiencia;
+    protected Status status;
+    
+    public Personagem(String nome){
+        this.nome = nome;
+        this.status = Status.VIVO;
+        this.inventario = new Inventario();
+    }
+
+    public void setNome(String n){
+        nome = n;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public Inventario getInventario(){
+        return inventario;
+    }
+
+    public int getExperiencia(){
+        return experiencia;
+    }
+
+    public Status getStatus(){
+        return status;
+    }
+
+}
