af658dac0865b90fe9f7feaede4aafdead46da77
Rafael Henrique da Silva <rafaelsilva192@gmail.com>
2016-10-27 17:29:27 -0200

    feat(MarioKart):Adicionar Corredor com Nivel de habilidade e nome

---
+++ Modulo 05/Exercicio 01/MarioKart/ClassLibrary1/Corredor.cs
@@ -0,0 +1,20 @@
+﻿using Corredores.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corredores
+{
+
+    public string[] Habilidade { get; }
+
+    public class Corredor(string[] habilidade)
+    {
+        this.Habilidade = 
+
+     }
+    
+}
+
