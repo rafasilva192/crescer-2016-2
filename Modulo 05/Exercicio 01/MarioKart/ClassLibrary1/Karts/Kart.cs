c899e8c23651e0ed244bf1210b6c4e9d7443e190
Rafael Henrique da Silva <rafaelsilva192@gmail.com>
2016-10-28 17:22:46 -0200

    feat(Mario Kart): Adicionar Karts e a possibilidade de equipar itens neles

---
+++ Modulo 05/Exercicio 01/MarioKart/ClassLibrary1/Karts/Kart.cs
@@ -0,0 +1,57 @@
+﻿using Corredores;
+using Corredores.Enums;
+using MarioKart.Itens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Karts
+{
+    public class Kart
+    {
+        private int velocidade;
+
+        public Kart(Corredor piloto)
+        {
+            this.Piloto = piloto;
+            this.Equipamentos = new List<IEquipamentos>();
+            this.velocidade = 3;
+        }
+
+        public List<IEquipamentos> Equipamentos { get;  set; }
+        public Corredor Piloto { get; private set; }
+        public int Velocidade
+        {
+            get
+            {
+                int velocidadePiloto = (int)Piloto.Habilidade;
+                bool pilotoEhProfissional = velocidadePiloto == 6;
+                velocidade += velocidadeDosEquipamentos(pilotoEhProfissional);
+                return velocidade + velocidadePiloto;
+            }
+            private set
+            {
+                this.velocidade = value;
+            }
+        }
+
+        public void Equipar(IEquipamentos equipamento)
+        {
+            this.Equipamentos.Add(equipamento);
+        }
+  
+
+        private int velocidadeDosEquipamentos(bool pilotoEhProfissional)
+        {
+            int bonusVelocidade = 0;
+            foreach (IEquipamentos item in this.Equipamentos)
+            {
+                if (pilotoEhProfissional) bonusVelocidade++;
+                 bonusVelocidade += item.BonusDeVelocidade;
+            }
+            return bonusVelocidade;
+        }
+    }
+}
---
+++ Modulo 05/Exercicio 01/MarioKart/CorredorTest/KartsTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Corredores;
+using Corredores.Enums;
+using MarioKart.Karts;
+using MarioKart.Itens;
+
+namespace CorredorTest
+{
+    [TestClass]
+    public class KartsTest
+    {
+        [TestMethod]
+        public void KartComPilotoNoobTem6DeVelocidade()
+        {
+            var piloto = new Corredor("Mario", NivelDeHabilidade.Noob);
+            var kart = new Kart(piloto);
+            Assert.AreEqual(6, kart.Velocidade);
+            Assert.AreEqual(piloto, kart.Piloto);
+        }
+        [TestMethod]
+        public void KartComPilotoMedianoEMotorTem11DeVelocidade()
+        {
+            var piloto = new Corredor("Mario", NivelDeHabilidade.Mediano);
+            var kart = new Kart(piloto);
+            kart.Equipar(new Motor_a_base_de_Lava());
+            Assert.AreEqual(11, kart.Velocidade);
+            Assert.AreEqual(piloto, kart.Piloto);
+        }
+        [TestMethod]
+        public void KartComPilotoProfissionalERodasDeDragaoTem12DeVelocidade()
+        {
+            var piloto = new Corredor("Mario", NivelDeHabilidade.Profissional);
+            var kart = new Kart(piloto);
+            kart.Equipar(new Pneus_de_Couro_de_Dragão());
+            Assert.AreEqual(12, kart.Velocidade);
+            Assert.AreEqual(piloto, kart.Piloto);
+        }
+        [TestMethod]
+        public void KartComPilotoProfissionalComRodasDeDragaoEMotorTem16DeVelocidade()
+        {
+            var piloto = new Corredor("Mario", NivelDeHabilidade.Profissional);
+            var kart = new Kart(piloto);
+            kart.Equipar(new Pneus_de_Couro_de_Dragão());
+            kart.Equipar(new Motor_a_base_de_Lava());
+            Assert.AreEqual(16, kart.Velocidade);
+            Assert.AreEqual(piloto, kart.Piloto);
+        }
+    }
+}
