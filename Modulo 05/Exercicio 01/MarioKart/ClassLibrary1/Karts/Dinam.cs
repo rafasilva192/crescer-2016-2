3b339e930a16323d27aced43c4e0c3c463e26221
Unknown <rafaelsilva192@gmail.com>
2016-10-28 23:33:33 -0200

    feat(Mario Kart): Adicionar Kart Dinam que dobra o bonus de velocidade do piloto

---
+++ Modulo 05/Exercicio 01/MarioKart/ClassLibrary1/Karts/Dinam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corredores;
+using Corredores.Enums;
+
+namespace MarioKart.Karts
+{
+    public class Dinam : Kart
+    {
+        public Dinam(Corredor piloto) : base(piloto)
+        {
+        }
+        protected override int velocidade
+        {
+            get
+            {
+                switch (Piloto.Habilidade)
+                {
+                    case NivelDeHabilidade.Mediano:
+                        return 5;
+                    case NivelDeHabilidade.Profissional:
+                        return 6 + Equipamentos.Count;
+                    default:
+                        return 3;
+                }
+
+            }
+        }
+    }
+}
