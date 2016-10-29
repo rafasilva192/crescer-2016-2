90f544454ee1b6cf0e43b907d1ee02e384ff39cd
Unknown <rafaelsilva192@gmail.com>
2016-10-29 00:14:56 -0200

    feat(Mario Kart): Adicionar Ultra Pack que pode receber outro item e recebe a velocidade desse item +20% como bonus

---
+++ Modulo 05/Exercicio 01/MarioKart/ClassLibrary1/Itens/UltraPack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Itens
+{
+    public class UltraPack : IEquipamentos
+    {
+        public UltraPack(IEquipamentos equipamento)
+        {
+            this.Equipamento = equipamento;
+        }
+
+        public int BonusDeVelocidade
+        {
+            get
+            {
+                double bonusVelocidade = Equipamento.BonusDeVelocidade + Math.Ceiling(Equipamento.BonusDeVelocidade*0.2);
+                return (int)bonusVelocidade;
+            }
+        }
+        private IEquipamentos Equipamento { get; set; }
+    }
+}
