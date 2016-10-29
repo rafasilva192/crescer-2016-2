507406950bccf688134c8dcb18f89c6aa91e20de
Unknown <rafaelsilva192@gmail.com>
2016-10-28 23:33:07 -0200

    feat(Mario Kart): Adicionar kart Light que recebe 3 de velocidade se o corredor for Noob

---
+++ Modulo 05/Exercicio 01/MarioKart/ClassLibrary1/Karts/Light.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corredores;
+using Corredores.Enums;
+
+namespace MarioKart.Karts
+{
+    public class Light : Kart
+    {
+        public Light(Corredor piloto) : base(piloto)
+        {
+        }
+        protected override int velocidade
+        {
+            get
+            {
+                switch (Piloto.Habilidade)
+                {
+                    case NivelDeHabilidade.Noob:
+                        return 3;
+                    case NivelDeHabilidade.Profissional:
+                        return -1;
+                    default:
+                        return 0;
+                }
+                
+            }
+        }
+    }
+}
