8a32b3069f9f45aeb480e312404973e681cfc6d3
Rafael Henrique da Silva <rafaelsilva192@gmail.com>
2016-10-31 17:36:28 -0200

    //TODO: Site Street Fighter terminar ficha técnica blanka e sobre

---
+++ Modulo 05/Exercicio 01/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,45 @@
+﻿using StreetFighter.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        // GET: StreetFighter
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            var model = new FichaTecnicaModel();
+
+            model.PrimeiraAparicao = "Street Fighter II The World Warrior (1991)";
+            model.Nascimento = new DateTime(12/02/1966);
+            model.Altura = 192;
+            model.Peso = 96;
+            model.Medidas = "B198, C120, Q172.";
+            model.TipoSanguineo = "B.";
+            model.HabilidadesEspeciais = "Caçar, Eletricidade.";
+            model.Gosta = "Frutas tropicais, Pirarucu, Sua mãe.";
+            model.Desgosta = "Army ants (espécie de formiga).";
+            model.EstiloLuta = "Luta Selvagem autodidata (Army Ants) / Capoeira.";
+            model.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia).";
+            model.FalaVitoria = "Ver você em ação é uma piada!";
+            model.SSF2Nickname = "A selvagem criança da natureza";
+            model.SFA3Nickname = "A animal pessoa amazônica";
+            model.SF4Nickname = "Guerreiro da selva";
+            model.SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?).";
+            model.SF2Stafe = "Bacia do rio Amazonas (Brasil).";
+            model.GolpesEspeciais = "Electric Thunder, Rolling Attack.";
+
+            return View(model);
+        }
+    }
+
+}
---
+++ Modulo 05/Exercicio 01/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Models
+{
+    public class FichaTecnicaModel : Controller
+    {
+        public string PrimeiraAparicao { get; set; }
+        public DateTime Nascimento { get; set; }
+        public int Altura { get; set; }
+        public int Peso { get; set; }
+        public string Medidas { get; set; }
+        public string TipoSanguineo { get; set; }
+        public string HabilidadesEspeciais { get; set; }
+        public string Gosta { get; set; }
+        public string Desgosta { get; set; }
+        public string EstiloLuta { get; set; }
+        public string Origem { get; set; }
+        public string FalaVitoria { get; set; }
+        public string SSF2Nickname { get; set; }
+        public string SFA3Nickname { get; set; }
+        public string SF4Nickname { get; set; }
+        public string SFA3Stage { get; set; }
+        public string SF2Stafe { get; set; }
+        public string GolpesEspeciais { get; set; }
+
+        // GET: FichaTecnicaModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
