72f8b78745d9c993650148be228d593020df587f
Unknown <rafaelsilva192@gmail.com>
2016-11-03 01:32:57 -0200

    feat(StreetFighter): Adicionar opção para cadastro de lutador na ficha técnica

---
+++ Modulo 05/Exercicio 04/StreetFighterCadastro/StreetFighterCadastro/Models/SobreModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Models
+{
+    public class SobreModel : Controller
+    {
+        public string Sobre { get; set; }
+    }
+}
